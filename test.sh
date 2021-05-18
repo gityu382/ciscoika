5dbe1c8deed03ea5ce1b7a26fd661a0e9fe17cd4
gityu382 <84359253+gityu382@users.noreply.github.com>
2021-05-18 12:00:37 +0900

    Update test.sh

--- test.sh
+++ test.sh
@@ -13,10 +13,10 @@ firewall-cmd --permanent --add-port=3128/tcp
 firewall-cmd --reload
 
 # Squid プロキシ ログを設定
-sed -i -e '$a \n# アクセスログ設定\nlogformat access_format %ts%03tu %<tt %>a %>p %>st %<A %<st %<la %<lp %la %lp %un %ru\naccess_log syslog:user.6 access_format' /etc/squid/squid.conf
+sed -i -e '$a # アクセスログ設定\nlogformat access_format %ts%03tu %<tt %>a %>p %>st %<A %<st %<la %<lp %la %lp %un %ru\naccess_log syslog:user.6 access_format' /etc/squid/squid.conf
 
 # Squid サーバのsyslog サービスを設定します
-sed -i -e '$a \n# Audit Log Facility BEGIN\nfilter bs_filter { filter(f_user) and level(info) };\ndestination udp_proxy { udp("10.1.100.181" port(514)); };\nlog {\nsource(s_all);\nfilter(bs_filter);\ndestination(udp_proxy);\n};\n# Audit Log Facility END' /etc/rsyslog.conf
+sed -i -e '$a # Audit Log Facility BEGIN\nfilter bs_filter { filter(f_user) and level(info) };\ndestination udp_proxy { udp("10.1.100.181" port(514)); };\nlog {\nsource(s_all);\nfilter(bs_filter);\ndestination(udp_proxy);\n};\n# Audit Log Facility END' /etc/rsyslog.conf
 
 # squid再起動
 systemctl restart squid
